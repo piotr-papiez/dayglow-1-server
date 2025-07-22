01d40842f5eda83fa5366193c0e5f406078f4556
Piotr Papież <piotr.a.papiez@gmail.com>
2025-07-22 20:28:04 +0200

    CORS update

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: "https://dayglow-1.onrender.com",
     credentials: true
 }));
 
