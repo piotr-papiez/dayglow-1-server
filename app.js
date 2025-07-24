27ac01770e5b1d5194fbdd2f4b198aff052af55e
Piotr Papież <piotr.a.papiez@gmail.com>
2025-07-24 20:04:26 +0200

    Token fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://dayglow-1-client.vercel.app",
+    origin: "https://dayglow-1.onrender.com",
     credentials: true
 }));
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -62,7 +62,7 @@ export const login = async (req, res) => {
         );
 
         res.cookie("token", token, {
-            httpOnly: true,
+            // httpOnly: true,
             secure: true,
             sameSite: "none",
             // domain: ".onrender.com",
