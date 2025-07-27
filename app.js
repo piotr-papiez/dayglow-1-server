83dd3e2fae6836b2a5337b793f12c5085cfee4e3
Piotr Papież <piotr.a.papiez@gmail.com>
2025-07-27 10:35:57 +0200

    Token fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3001",
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
